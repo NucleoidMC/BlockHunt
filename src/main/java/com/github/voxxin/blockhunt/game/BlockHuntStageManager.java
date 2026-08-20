0ecabcfcb16ea101be0f438844df67bfe1e766ca
ellieisjelly <ellie@ellieis.me>
2026-08-20 17:41:22 -0300

    Fix finish time not being offset by the starting time

--- src/main/java/com/github/voxxin/blockhunt/game/BlockHuntStageManager.java
+++ src/main/java/com/github/voxxin/blockhunt/game/BlockHuntStageManager.java
@@ -34,7 +34,7 @@ public class BlockHuntStageManager {
     public void onOpen(long time, BlockHuntConfig config, long seekerRelease) {
         this.startTime = timeCalc(time, 0, 3);
         this.seekersRelease = seekerRelease + this.startTime;
-        this.finishTime = config.endTime();
+        this.finishTime = time + config.endTime();
     }
 
     public IdleTickResult tick(long time, GameSpace space) {
